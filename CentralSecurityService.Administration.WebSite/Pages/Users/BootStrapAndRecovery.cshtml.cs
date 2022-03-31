af003e823ba755bc18a1a356a230a1d603e17da6
Eamonn Duffy <Eamonn@Duffy.global>
2022-03-31 12:20:17 +0100

    Placeholder Users Boot Strap And Recovery Page.

---
+++ CentralSecurityService.Administration.WebSite/Pages/Users/BootStrapAndRecovery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CentralSecurityService.Administration.WebSite.Pages
+{
+    public class BootStrapAndRecoveryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
